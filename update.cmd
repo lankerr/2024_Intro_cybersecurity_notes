d231af86d72cb3ad4e6cd0f6676a32c9d5b8888f
你的名字 <lankerr>
2024-05-07 02:21:44 +0800

    update 2024050702hour21minute44second

--- update.cmd
+++ update.cmd
@@ -6,7 +6,7 @@ set "day=%dt:~6,2%"
 set "hour=%dt:~8,2%"
 set "minute=%dt:~10,2%"
 set "second=%dt:~12,2%"
-set "timestamp=%year%年%month%月%day%日%hour%时%minute%分%second%秒"
+set "timestamp=%year%%month%%day%%hour%hour%minute%minute%second%second"
 git add . --all
 git commit -m "update %timestamp%"
 git push
